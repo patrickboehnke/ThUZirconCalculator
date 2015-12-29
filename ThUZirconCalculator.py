a9d12c497ff208d98c666a6bfe82d18132056713
patrickboehnke <pboehnke@ucla.edu>
2015-12-28 22:00:33 -0500

    Update ThUZirconCalculator.py

--- ThUZirconCalculator.py
+++ ThUZirconCalculator.py
@@ -6,25 +6,12 @@ Created on Thu Dec 28 19:51:54 2015
 
 If you use this code please cite:
 
-Boehnke, P., Barboni, M., & Bell, E. A. (2015). Zircon U/Th Model Ages in the Presence of Melt Heterogeneity. Quaternary Geochronology, Submitted.
+Boehnke, P., Barboni, M., & Bell, E. A. (2016). Zircon U/Th Model Ages in the Presence of Melt Heterogeneity. Quaternary Geochronology, Submitted.
 
 For comments please contact: pboehnke @ gmail . com (remove spaces)
 """
 
 import numpy as np
-import copy
-
-#This loads the data file please change it to the right file name
-#Data file must be in same directory as this python script
-Data = np.loadtxt("Example.csv", delimiter=",")
-U238Th232 = Data[:, 0]
-U238Th2321sig = Data[:, 1]
-Th230Th232 = Data[:, 2]
-Th230Th2321sig = Data[:, 3]
-
-ModelSlope = copy.deepcopy(U238Th232)
-ModelSlopeUnc = copy.deepcopy(U238Th232)
-EquiPoint = np.zeros(len(U238Th232))
 
 #Partition Coefficients for U relative to Th
 #Value calculated and justified in manuscript
@@ -32,19 +19,37 @@ EquiPoint = np.zeros(len(U238Th232))
 DUDTh = 7
 DUDThunc = 0.4 #Uncertainty at 1 sigma
 
-for iter1 in range(len(U238Th232)):
-    NewSlope = np.zeros(1000)
-    for iter2 in range(0,1000):
-        Slope = 1.1
-        while(Slope > 1):
-            U238Th232Samp = np.random.randn()*U238Th2321sig[iter1] + U238Th232[iter1]
-            Th230Th232Samp = np.random.randn()*Th230Th2321sig[iter1] + Th230Th232[iter1]
-            PartCoef = np.random.randn()*DUDThunc + DUDTh
-            U238Th232M = U238Th232Samp/PartCoef
-            Th230Th232M = np.random.randn()*0.16*U238Th232M + U238Th232M
-            
-            Slope = (Th230Th232Samp - Th230Th232M)/(U238Th232Samp - U238Th232M)
-        NewSlope[iter2] = Slope
+#This sets the number of samples for each sample calculation
+#Higher is better but takes longer, 1000 is the default
+NumIterations = 1000
+
+#This loads the data file please change it to the right file name
+#Data file must be in same directory as this python script
+Data = np.loadtxt("Example.csv", delimiter=",")
+
+U238Th232 = Data[:, 0]
+U238Th2321sig = Data[:, 1]
+Th230Th232 = Data[:, 2]
+Th230Th2321sig = Data[:, 3]
+
+ModelSlope = np.copy(U238Th232)
+ModelSlopeUnc = np.copy(U238Th232)
+NewSlope = np.zeros(NumIterations)
+
+
+for iter1 in range(len(U238Th232)):    
+    for iter2 in range(NumIterations):
+        U238Th232Samp = np.random.randn()*U238Th2321sig[iter1] + U238Th232[iter1]
+        Th230Th232Samp = np.random.randn()*Th230Th2321sig[iter1] + Th230Th232[iter1]
+        PartCoef = np.random.randn()*DUDThunc + DUDTh
+        U238Th232M = U238Th232Samp/PartCoef
+        Th230Th232M = np.random.randn()*0.16*U238Th232M + U238Th232M
+        
+        Slope = (Th230Th232Samp - Th230Th232M)/(U238Th232Samp - U238Th232M)
+        if Slope > 1:
+            NewSlope[iter2] = 1
+        else:
+            NewSlope[iter2] = Slope
     ModelSlope[iter1] = np.mean(NewSlope)
     ModelSlopeUnc[iter1] = np.std(NewSlope)
 
